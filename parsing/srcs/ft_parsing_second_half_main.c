b306d72252b549e9df40394f5d6068d23c5433af
Adrien <adrien.consta@gmail.com>
2021-02-02 11:25:21 +0100

    auto-commit

--- parsing/srcs/ft_parsing_second_half_main.c
+++ parsing/srcs/ft_parsing_second_half_main.c
@@ -6,7 +6,7 @@
 /*   By: calao <adconsta@student.42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 16:40:47 by calao             #+#    #+#             */
-/*   Updated: 2021/02/02 10:37:48 by calao            ###   ########.fr       */
+/*   Updated: 2021/02/02 11:25:02 by calao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,18 @@ int		ft_make_map(t_cube *map);
 void	ft_copy_line_to_map(char *line, char **map, int max_col);
 int		ft_wall_close_hze_check(char **map, int max_col, int max_row);
 int		ft_wall_closed_vert_check(char **map, int max_row);
+int		ft_zero_in_void_check(char **map);
 
 int		ft_second_parsing(int fd, t_cube *map_info)
 {
 	if (ft_make_oneline_map(fd, map_info) == -1)
-		return (-1);	
+		return (-1);
+	printf("making map....\n");
 	if (ft_make_map(map_info) == -1)
 		return (-1);
+	printf("copying line to map...\n");
 	ft_copy_line_to_map(map_info->m_line, map_info->map, map_info->max_col);
+	printf("checking edges & void ...\n");
 	if (ft_check_fullmap_format(map_info->map, map_info->max_col, map_info->max_row) == -1)
 		return (-1);
 	return (1);
@@ -49,6 +53,7 @@ int		ft_make_map(t_cube *map)
 		map->map[i][map->max_col] = '\0';
 		i++;
 	}
+	printf("m_row = %d | m_col = %d\n", map->max_row, map->max_col);
 	return (1);
 }
 
@@ -60,7 +65,7 @@ void	ft_copy_line_to_map(char *line, char **map, int max_col)
 
 	i = 0;
 	row = 0;
-	while (line[i] != '\0')
+	while (map[row] != NULL && line[i] != '\0')
 	{
 		col = 0;
 		while (line[i] != '@' && line[i] != '\0')
@@ -74,8 +79,8 @@ void	ft_copy_line_to_map(char *line, char **map, int max_col)
 			map[row][col] = ' ';
 			col++;
 		}
-		row++;
 		i++;
+		row++;
 	}
 }
 
@@ -150,50 +155,44 @@ int		ft_wall_closed_vert_check(char **map, int max_row)
 	}
 	return (0);
 }
+
+int		ft_zero_in_void_check(char **map)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (map[i] != NULL)
+	{
+		j = 0;
+		while (map[i][j] != '\0')
+		{
+			if (map[i][j] != '1' && map[i][j] != ' ')
+			{
+				if (map[i - 1][j] == ' ' || map[i + 1][j] == ' ')
+					return (1);
+				if (map[i][j - 1] == ' ' || map[i][j + 1] == ' ')
+					return (1);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
 int		ft_check_fullmap_format(char **map, int max_col, int max_row)
 {
 	(void)max_row;
 	if (ft_emptyline_vert_check(map))
-	{
-		printf("\nVERTICAL EMPTY LINES!\n");
 		return (-1);
-	}
 	if (ft_wall_closed_hze_check(map, max_col) 
 			|| ft_wall_closed_vert_check(map, max_row))
+		return (-1);
+	if (ft_zero_in_void_check(map))
 	{
-		printf("ERROR WALL not close properly\n");
+		printf("\t***ZERO_IN_VOID***\n");
 		return (-1);
 	}
-	/*
-	if (ft_zero_in_void_check(map))
-		return (-1);
-		*/
 	return (1);
 }
-	
-/*
- 
-int		ft_empty_line_vert_check(char **map)
-{
-	int row;
-	int col;
-	int only_space;
-
-	col = 0;
-	while (map[0][col] != '\0')
-	{
-		row = 0;
-		only_space = TRUE;
-		while (map[row][col] != '\0)
-			return (0);
-	}
-}
-
-int		ft_wall_closed_hze_check(char **map);
-int		ft_wall_closed_vert_check(char **map);
-int		ft_zero_in_void_check(char **map);
-
-
-
-
-*/
